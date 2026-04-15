441be196f5627dcb07773fb39137ebd290be0274
Kimetz Milan <kim_mil_apa@goierrieskola.org>
2026-04-15 09:34:09 +0200

    Clasearen izena aldatzen errorea konpontzeko

---
+++ Balorazioa.cs
@@ -0,0 +1,95 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Erronka3
+{
+    internal class Balorazioa
+    {
+        private double puntuazioa; private String izena; public Balorazioak(String izena, double puntuazioa)
+
+        {
+
+            this.izena = izena;
+
+            this.puntuazioa = puntuazioa;
+
+        }
+        public static List<Balorazioak> webgunekoBalorazioak(int modua, DatuBaseKonexioa conn)
+
+        {
+
+            MySqlConnection konexioa = conn.getKonexioa();
+
+            konexioa.Open();
+
+            try
+            {
+
+                String ordenatu;
+
+                List<Balorazioak> lista = new List<Balorazioak>();
+
+                if (modua == 1)
+
+                {
+
+                    ordenatu = "V_Balorazio_Hobenak";
+
+                }
+
+                else
+                {
+
+                    ordenatu = "V_Balorazio_Txarrenak";
+
+                }
+
+                MySqlCommand iritziak = new MySqlCommand("select * from " + ordenatu + " order by puntuazioaAVG desc;", konexioa);
+
+                MySqlDataReader asko = iritziak.ExecuteReader();
+
+                while (asko.Read())
+
+                {
+
+                    Balorazioak b = new Balorazioak(Convert.ToString(asko[0]), Convert.ToDouble(asko[1]));
+
+                    lista.Add(b);
+
+                }
+
+                return lista;
+
+            }
+
+            catch (Exception e)
+
+            {
+
+                MessageBox.Show(Convert.ToString(e));
+
+                return null;
+
+            }
+
+        }//Getterpublic
+         String getIzena()
+
+{
+
+    return this.izena;
+
+}public Double getPuntuazioa()
+
+        {
+
+            return this.puntuazioa;
+
+        }
+    }
+}
---
+++ DatuBaseKonexioa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Erronka3
+{
+    internal class DatuBaseKonexioa
+    {
+        private String konexioa = "Server=192.168.115.159;Database=3erronka;User Id=admin;Password=1MG32025;";
+
+        //private String konexioa = "Server=localhost;Database=erronka3;User Id=root;Password=1MG32025;";
+        public MySqlConnection getKonexioa()
+
+{
+
+    MySqlConnection con = new MySqlConnection(this.konexioa);
+
+    return con;
+
+}
+}
+}
