7fa4cfa89e4cc2d1d182835813a717ab691e6bb2
Kimetz Milan <kim_mil_apa@goierrieskola.org>
2026-04-15 08:35:59 +0200

    Datu basearen konexioa sortzen

---
+++ DatuBaseKonexioa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace _3_Erronka
+{
+    internal class DatuBaseKonexioa
+    {
+        private String konexioa = "Server=192.168.115.159;Database=3erronka;User Id=admin;Password=1MG32025;";
+
+        //private String konexioa = "Server=localhost;Database=erronka3;User Id=root;Password=1MG32025;";
+        public MySqlConnection getKonexioa(){
+
+        MySqlConnection con = new MySqlConnection(this.konexioa);
+
+        return con;
+        }
+    }
+}
